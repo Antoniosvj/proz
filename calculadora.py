ebf0c35846437a2853ad0d8cfa4b798d7c3b1402
Antoniosvj <118409406+Antoniosvj@users.noreply.github.com>
2024-04-20 11:07:19 -0300

    Calculadora atualizada

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,27 @@
-num1 = int(input('Digite o primeiro número: '))
-num2 = int(input('Digite o segundo número: '))
-operacao = int(input('''Qual tipo de operação deseja fazer:
-                     1 . Soma
-                     2 . Subtração
-                     3 . Multiplicação
-                     4 . Divisão \n'''))
-def calculadora(num1, num2, operacao):
-    if operacao == 1:
-        return num1 + num2
-    elif operacao == 2:
-        return num1 - num2
-    elif operacao == 3:
-        return num1 * num2
-    elif operacao == 4:
-        return num1 / num2
+operacao = 1
+while operacao != 0:
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o segundo número: '))
+    operacao = int(input('''Qual tipo de operação deseja fazer:
+                        1 . Soma
+                        2 . Subtração
+                        3 . Multiplicação
+                        4 . Divisão
+                        0 . Sair\n'''))
+    if operacao == 0:
+        print('Obrigado por ter utilizado nossa calculadora!!!')
     else:
-        return 'Operação inválida'
-    
-resultado = calculadora(num1, num2, operacao)
-print(" o resultado da operação selecionada é: ", resultado)
+        def calculadora(num1, num2, operacao):
+            if operacao == 1:
+                return num1 + num2
+            elif operacao == 2:
+                return num1 - num2
+            elif operacao == 3:
+                return num1 * num2
+            elif operacao == 4:
+                return num1 / num2
+            else:
+                return 'Operação inválida'
+            
+        resultado = calculadora(num1, num2, operacao)
+        print(" o resultado da operação selecionada é: ", resultado)
